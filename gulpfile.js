dc31162441941237eeb704882b0925e605bb3314
Uriel thoma <108682061+UrielCT@users.noreply.github.com>
2023-01-27 14:38:07 -0300

    Conversión de imágenes .png a .webp/.jpg/.avif

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-const { src, dest, watch } =require("gulp");
+const { src, dest, watch, parallel } =require("gulp");
+
+//css
 const sass = require("gulp-sass")(require('sass'));
 const plumber = require('gulp-plumber');
+
+// Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done){
     src("src/scss/**/*.scss") //identifica archivo de sass
@@ -11,9 +19,50 @@ function css(done){
     done();  // Callback que avisa a gulp cuando llego al final
 }
 
+
+function imagenes(done){
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'))
+    done(); 
+}
+
+function versionWebp(done){
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+
+    done(); 
+}
+
+
+function versionAvif(done){
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones))
+        .pipe(dest('build/img'))
+
+    done(); 
+}
+
 function dev(done){
     watch("src/scss/**/*.scss", css)
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp= versionWebp;
+exports.versionAvif= versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
